0e2144a2990a638072c5dc56e3fde987995e8ef0
Leonardo Correia <128181092+Leonardo-Correia-da-silva@users.noreply.github.com>
2024-01-23 00:06:03 -0300

    Update projetos.js

--- src/componentes/container/projetos/projetos.js
+++ src/componentes/container/projetos/projetos.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import "./projetos.css"
-import Projeto from '../../../projeto1.png';
+import Imagem1 from '../../../timezone.png';
+import Imagem2 from '../../../projeto1.png';
+import Imagem3 from '../../../portfolio.png';
+
+
 
 function Projetos () {
     return (
@@ -9,13 +13,31 @@ function Projetos () {
             
             <h1 className="projetos">Projetos</h1>
             <div className="container-projeto">
+               
+            <div className="projeto1">
+                <img src={Imagem1} className="imagem-projeto" alt="img" />
+                    <h2 className="titulo-projetos">Timezone App</h2>
+                    <p className="p-projetos">Aplicativo inovador que permite visualizar instantaneamente os horários atuais em diversas regiões do mundo. </p> <br></br>
+                    <p className="p-links">Repositório: <Link to="https://github.com/Leonardo-Correia-da-silva/Timezone-App" target="_blank" style={{  textDecoration: 'none'  }} > <span className="click" > Clique aqui </span> </Link> </p>
+                    <p className="p-links">Aplicação no ar: <Link to="https://timezone-app-alpha.vercel.app/" target="_blank" style={{  textDecoration: 'none'  }}>  <span className="click">Clique aqui</span> </Link> </p>
+                </div>
+               
                 <div className="projeto1">
-                <img src={Projeto} className="imagem-projeto" alt="img" />
-                    <h2 className="titulo-projetos">Building Material calculator</h2>
+                <img src={Imagem2} className="imagem-projeto" alt="img" />
+                    <h2 className="titulo-projetos">Portfólio</h2>
+                    <p className="p-projetos"> Esta aplicação web tem a capacidade de estimar uma quantidade  aproximada de material de construção!</p> <br></br>
+                    <p className="p-links">Repositório: <Link to="https://github.com/Leonardo-Correia-da-silva/Portfolio" target="_blank" style={{  textDecoration: 'none'  }} > <span className="click" > Clique aqui </span> </Link> </p>
+                    <p className="p-links">Aplicação no ar: <Link style={{  textDecoration: 'none'  }}>  <span className="click">Você já está aqui</span> </Link> </p>
+                </div>
+              
+                <div className="projeto1">
+                <img src={Imagem3} className="imagem-projeto" alt="img" />
+                    <h2 className="titulo-projetos">Building Material Calculator</h2>
                     <p className="p-projetos"> Esta aplicação web tem a capacidade de estimar uma quantidade  aproximada de material de construção!</p> <br></br>
                     <p className="p-links">Repositório: <Link to="https://github.com/Leonardo-Correia-da-silva/Building-material-calculator" target="_blank" style={{  textDecoration: 'none'  }} > <span className="click" > Clique aqui </span> </Link> </p>
                     <p className="p-links">Aplicação no ar: <Link to="https://building-material-calculator.vercel.app/" target="_blank" style={{  textDecoration: 'none'  }}>  <span className="click">Clique aqui</span> </Link> </p>
                 </div>
+                <br></br><br></br><br></br>
             </div>
         </main>
 
